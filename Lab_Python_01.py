0902a4e594b9882eb516769008020e472861dfb7
mimi <mimi@mimi-pc.(none)>
2012-06-22 14:11:21 +0000

    hello

---
+++ Lab_Python_01.py
@@ -0,0 +1,41 @@
+
+1. x, length, NUMBER, Android_phone 1
+bool
+float
+str
+int
+str
+int
+error
+bool
+str
+str
+
+2.
+False
+True
+True
+True
+False
+True
+
+3.
+2
+2.5
+2.5
+0
+3.5
+25
+25.0
+25.0
+0
+0.33333333333333
+
+4.
+fst_name= raw_input("Enter your fist name:")
+lst_name= raw_input("Enter your last name:")
+print "Enter your date of birth"
+mnth= raw_input("Month?")
+day= raw_input("Day?")
+yr= raw_input("Year?")
+ print fst_name+" "+lst_name+" "+mnth+" "+day+" "+yr
